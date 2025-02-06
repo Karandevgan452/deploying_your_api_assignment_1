e28db8e64c09f72264e3815f749e2642b71fc9a5
name <karankarandevgan@gmail.com>
2025-02-06 10:57:17 +0530

    Assignment completed

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const express = require('express');
-const { resolve } = require('path');
+const express = require("express");
+const { resolve } = require("path");
+require("dotenv").config();
 
 const app = express();
-const port = 3010;
+const port = 7584;
 
-app.use(express.static('static'));
+const config = {
+  apiKey: process.env.API_KEY,
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  serverSecret: process.env.SERVER_SECRET,
+
+  isKalvian: process.env.IS_KALVIAN === "true",
+};
+
+console.log(config);
+
+app.use(express.static("static"));
+
+app.get("/", (req, res) => {
+  res.sendFile(resolve(__dirname, "pages/index.html"));
 });
 
 app.listen(port, () => {
